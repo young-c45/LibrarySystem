19bc683c55364953e67975f7fd98b2f41ec98951
Patrick White <white-p13@ulster.ac.uk>
2022-03-15 11:34:49 +0000

    Item Framework

--- src/librarysystem/Item.java
+++ src/librarysystem/Item.java
@@ -8,5 +8,32 @@
 package librarysystem;
 
 public class Item {
+    
+   private String barcode ;
+   private String author ;
+   private String title ;
+   private String type ;
+   private String year;
+   private String isbn;
+   
+   public Item(String newBarcode, String newAuthor, String newTitle, String newType, String newYear, String newIsbn){
+       barcode = newBarcode; 
+       author = newAuthor;
+       title = newTitle;
+       type = newType;
+       year = newYear;
+       isbn = newIsbn;
+   }
+         
+   public String getBarcode(){
+       return barcode;
+   }
+
+  public void displayItem(){
+      System.out.println("Displayed Item");
+  }
+          
+
 
 }
+
