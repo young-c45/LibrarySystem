5b74a805a5ae9d6889f9db88c0cd8256f58ea9da
Callum Young <young-c45@ulster.ac.uk>
2022-03-29 12:55:38 +0100

    Coded displayItem logic

--- src/librarysystem/Item.java
+++ src/librarysystem/Item.java
@@ -4,36 +4,37 @@
  * B-number: B00798718
  * Created: 15/03/2022
  */
-
 package librarysystem;
 
 public class Item {
-    
-   private String barcode ;
-   private String author ;
-   private String title ;
-   private String type ;
-   private String year;
-   private String isbn;
-   
-   public Item(String newBarcode, String newAuthor, String newTitle, String newType, String newYear, String newIsbn){
-       barcode = newBarcode; 
-       author = newAuthor;
-       title = newTitle;
-       type = newType;
-       year = newYear;
-       isbn = newIsbn;
-   }
-         
-   public String getBarcode(){
-       return barcode;
-   }
 
-  public void displayItem(){
-      System.out.println("Displayed Item");
-  }
-          
+    private String barcode;
+    private String author;
+    private String title;
+    private String type;
+    private String year;
+    private String isbn;
 
+    public Item(String newBarcode, String newAuthor, String newTitle, String newType, String newYear, String newIsbn) {
+        barcode = newBarcode;
+        author = newAuthor;
+        title = newTitle;
+        type = newType;
+        year = newYear;
+        isbn = newIsbn;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void displayItem() {
+        System.out.println("\tItem Barcode: " + barcode);
+        System.out.println("\tAuthor: " + author);
+        System.out.println("\tTitle: " + title);
+        System.out.println("\tItem Type: " + type);
+        System.out.println("\tYear Published: " + year);
+        System.out.println("\tISBN: " + isbn);
+    }
 
 }
-
